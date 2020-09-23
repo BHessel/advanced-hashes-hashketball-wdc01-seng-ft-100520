ffa9a30bf66a77b423c5af57536ca430572306cd
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-23 21:15:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,7 +189,7 @@ def player_with_largest_shoes
   binding.pry
   player_list.max_by do |shoe_size|
     shoe_size[:shoe]
-    shoe_size[:shoe][:name]
+    #shoe_size[:shoe][:name]
   end
   #binding.pry
 end
